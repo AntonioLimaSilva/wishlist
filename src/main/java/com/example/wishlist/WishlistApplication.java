44a673db9c0e6c38e7570c4de140e03bbbcbd88d
ext.aluciano <ext.aluciano@unicid.edu.br>
2023-06-01 08:33:43 -0300

    remove implementacao teste

--- src/main/java/com/example/wishlist/WishlistApplication.java
+++ src/main/java/com/example/wishlist/WishlistApplication.java
@@ -1,24 +1,13 @@
 package com.example.wishlist;
 
-import com.example.wishlist.gateway.mongodb.entity.product.ProductEntity;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class WishlistApplication implements CommandLineRunner {
-
+public class WishlistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WishlistApplication.class, args);
-	}
-
-
-	@Override
-	public void run(String... args) {
-		ProductEntity productEntity = new ProductEntity();
-
-//		wishlistService.add(Collections.singletonList(product));
 	}
 }
